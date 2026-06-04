f4505fc618ddaaa03e69f9901c6689d30bcc1377
Juan Fausta Pringadi <93523229+juanfaustaa@users.noreply.github.com>
2026-06-04 15:18:32 +0700

    Initial Commit

---
+++ TestGitHub/TestGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitHubApp.swift
+//  TestGitHub
+//
+//  Created by Juan Fausta Pringadi on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TestGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
